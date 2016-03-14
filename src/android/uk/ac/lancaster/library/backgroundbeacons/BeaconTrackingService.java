6418c202af6ba29340c9e3f43ea28c03d4e59d1f
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-03-14 09:48:33 +0000

    Updated all tracking service event methods to respect send movement data preference

--- src/android/uk/ac/lancaster/library/backgroundbeacons/BeaconTrackingService.java
+++ src/android/uk/ac/lancaster/library/backgroundbeacons/BeaconTrackingService.java
@@ -30,108 +30,114 @@ public class BeaconTrackingService {
 
   public void EnterRegionEvent(BeaconTrackingEvent event) {
 
-    try {
+    if (this.settings.getSendMovementData()) {
+      try {
 
-      URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons/regions");
+        URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons/regions");
 
-      String json = event.toJsonObject().toString();
+        String json = event.toJsonObject().toString();
 
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setReadTimeout(10000);
-      conn.setConnectTimeout(15000);
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setFixedLengthStreamingMode(json.getBytes().length);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setFixedLengthStreamingMode(json.getBytes().length);
 
-      conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-      conn.setRequestProperty("Accept", "application/json");
-      conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
-      conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
+        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
+        conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
 
-      conn.connect();
+        conn.connect();
 
-      OutputStream os = new BufferedOutputStream(conn.getOutputStream());
-      os.write(json.getBytes());
-      os.flush();
+        OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+        os.write(json.getBytes());
+        os.flush();
 
-      os.close();
-      conn.disconnect();
+        os.close();
+        conn.disconnect();
 
-    } catch (Exception e) {
+      } catch (Exception e) {
 
+      }
     }
 
   }
 
   public void ExitRegionEvent(BeaconTrackingEvent event) {
 
-    try {
+    if (this.settings.getSendMovementData()) {
+      try {
 
-      URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons/regions");
+        URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons/regions");
 
-      String json = event.toJsonObject().toString();
+        String json = event.toJsonObject().toString();
 
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setReadTimeout(10000);
-      conn.setConnectTimeout(15000);
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setFixedLengthStreamingMode(json.getBytes().length);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setFixedLengthStreamingMode(json.getBytes().length);
 
-      conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-      conn.setRequestProperty("Accept", "application/json");
-      conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
-      conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
+        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
+        conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
 
-      conn.connect();
+        conn.connect();
 
-      OutputStream os = new BufferedOutputStream(conn.getOutputStream());
-      os.write(json.getBytes());
-      os.flush();
+        OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+        os.write(json.getBytes());
+        os.flush();
 
-      os.close();
-      conn.disconnect();
+        os.close();
+        conn.disconnect();
 
-    } catch (Exception e) {
+      } catch (Exception e) {
 
+      }
     }
 
   }
 
   public void RangeBeaconEvent(BeaconTrackingEvent event) {
 
-    try {
+    if (this.settings.getSendMovementData()) {
+      try {
 
-      URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons");
+        URL url = new URL(this.apiUrl + this.apiVersion + "/tracking/locations/beacons");
 
-      String json = event.toJsonObject().toString();
+        String json = event.toJsonObject().toString();
 
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setReadTimeout(10000);
-      conn.setConnectTimeout(15000);
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setFixedLengthStreamingMode(json.getBytes().length);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setFixedLengthStreamingMode(json.getBytes().length);
 
-      conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-      conn.setRequestProperty("Accept", "application/json");
-      conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
-      conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
+        conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("X-USER-EMAIL", this.settings.getApiUser());
+        conn.setRequestProperty("X-USER-TOKEN", this.settings.getApiToken());
 
-      conn.connect();
+        conn.connect();
 
-      OutputStream os = new BufferedOutputStream(conn.getOutputStream());
-      os.write(json.getBytes());
-      os.flush();
+        OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+        os.write(json.getBytes());
+        os.flush();
 
-      os.close();
-      conn.disconnect();
+        os.close();
+        conn.disconnect();
 
-    } catch (Exception e) {
+      } catch (Exception e) {
 
+      }
     }
 
   }
